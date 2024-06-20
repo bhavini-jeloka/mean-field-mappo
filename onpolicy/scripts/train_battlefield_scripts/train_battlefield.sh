bad63870ce80ce27d55ef85e9a7b56b82503a428
bhavini-jeloka <jelokab@gmail.com>
2024-06-20 12:02:27 -0400

    tune hyperparams

--- onpolicy/scripts/train_battlefield_scripts/train_battlefield.sh
+++ onpolicy/scripts/train_battlefield_scripts/train_battlefield.sh
@@ -9,7 +9,7 @@ for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
     CUDA_VISIBLE_DEVICES=0 python ../train/train_battlefield.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
-    --seed ${seed} --n_training_threads 1 --n_rollout_threads 128 --num_mini_batch 5 --episode_length 100 \
+    --seed ${seed} --n_training_threads 1 --n_rollout_threads 64 --num_mini_batch 40 --episode_length 50 \
     --num_env_steps 2000000 --ppo_epoch 10 --gain 0.01 --lr 7e-4 --critic_lr 7e-4 --wandb_name "xxx" \
     --user_name "yuchao" 
 done
