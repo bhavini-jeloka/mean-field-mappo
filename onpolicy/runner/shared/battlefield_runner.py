a25d5b5d3fce3920c6d98d4e8d7972fd3990226c
bhavini-jeloka <jelokab@gmail.com>
2024-06-19 21:16:56 -0400

    print shape of share obs

--- onpolicy/runner/shared/battlefield_runner.py
+++ onpolicy/runner/shared/battlefield_runner.py
@@ -81,12 +81,12 @@ class BattleFieldRunner(Runner):
     def warmup(self):
         # reset env
         obs = self.envs.reset()
-        print(obs.shape)
 
         # replay buffer
         if self.use_centralized_V:
             share_obs = obs.reshape(self.n_rollout_threads, -1)
             share_obs = np.expand_dims(share_obs, 1).repeat(self.num_agents, axis=1)
+            print(share_obs.shape)
         else:
             share_obs = obs
 
