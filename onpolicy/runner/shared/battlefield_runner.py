30e90989710996104747f44a9f5feae558b0b4c8
bhavini-jeloka <jelokab@gmail.com>
2024-06-19 21:48:16 -0400

    check one hot encoding

--- onpolicy/runner/shared/battlefield_runner.py
+++ onpolicy/runner/shared/battlefield_runner.py
@@ -31,6 +31,9 @@ class BattleFieldRunner(Runner):
                 actions_env = np.argmax(actions_env, axis=-1)
                 actions_env = np.expand_dims(actions_env, axis=-1)
                 print('about to step')
+                print(actions_env.shape)
+                print(actions_env)
+                assert(1==0)
                 obs, rewards, dones, infos = self.envs.step(actions_env)
                 print('finish step')
 
