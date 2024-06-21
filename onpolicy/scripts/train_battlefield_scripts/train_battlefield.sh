a262f1c77c6da056c92d5ecc3bd29923bd55194c
bhavini-jeloka <jelokab@gmail.com>
2024-06-21 13:37:08 -0400

    correct tuning

--- onpolicy/scripts/train_battlefield_scripts/train_battlefield.sh
+++ onpolicy/scripts/train_battlefield_scripts/train_battlefield.sh
@@ -10,6 +10,6 @@ do
     echo "seed is ${seed}:"
     CUDA_VISIBLE_DEVICES=0 python ../train/train_battlefield.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
     --seed ${seed} --n_training_threads 1 --n_rollout_threads 64 --num_mini_batch 40 --episode_length 50 \
-    --num_env_steps 2000000 --ppo_epoch 10 --gain 0.01 --lr 7e-4 --critic_lr 7e-4 --wandb_name "xxx" \
+    --num_env_steps 20000000 --ppo_epoch 10 --gain 0.01 --lr 7e-4 --critic_lr 7e-4 --wandb_name "xxx" \
     --user_name "yuchao" 
 done
