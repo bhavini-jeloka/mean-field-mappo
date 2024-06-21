950eb7e899dc759e4a10b22c0eaff232fdd5a114
bhavini-jeloka <jelokab@gmail.com>
2024-06-21 12:08:34 -0400

    add running script

--- onpolicy/scripts/render_battlefield.sh
+++ onpolicy/scripts/render_battlefield.sh
@@ -8,7 +8,7 @@ echo "env is ${env},  algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=0 python animationMAPPO.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
+    CUDA_VISIBLE_DEVICES=0 python3 animationMAPPO.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
     --seed ${seed} --n_training_threads 1 --n_rollout_threads 64 --num_mini_batch 40 --episode_length 50 \
     --num_env_steps 2000000 --ppo_epoch 10 --gain 0.01 --lr 7e-4 --critic_lr 7e-4 --wandb_name "xxx" \
     --user_name "yuchao" 
