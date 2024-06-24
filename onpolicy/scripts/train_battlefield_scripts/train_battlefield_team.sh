4648c3f28ad76f4f20316f23b7721bcf5df4febf
bhavini-jeloka <jelokab@gmail.com>
2024-06-23 22:59:26 -0400

    revert

--- onpolicy/scripts/train_battlefield_scripts/train_battlefield_team.sh
+++ onpolicy/scripts/train_battlefield_scripts/train_battlefield_team.sh
@@ -10,6 +10,6 @@ do
     echo "seed is ${seed}:"
     CUDA_VISIBLE_DEVICES=0 python ../train/train_battlefield_team.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
     --seed ${seed} --n_training_threads 1 --n_rollout_threads 4 --num_mini_batch 40 --episode_length 50 \
-    --num_env_steps 2000 --ppo_epoch 10 --gain 0.01 --lr 7e-4 --critic_lr 7e-4 --wandb_name "xxx" \
+    --num_env_steps 2000000 --ppo_epoch 10 --gain 0.01 --lr 7e-4 --critic_lr 7e-4 --wandb_name "xxx" \
     --user_name "yuchao" 
 done
